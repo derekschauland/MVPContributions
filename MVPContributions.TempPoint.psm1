ac2f3381b6c85b23848d8ef17a52dd34693fcbe6
Derek Schauland <dschauland@weareinnova.com>
2017-02-01 14:50:25 -0600

    Initial Commit

---
+++ MVPContributions.TempPoint.psm1
@@ -0,0 +1,23 @@
+﻿<#	
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2017 v5.4.134
+	 Created on:   	2/1/2017 1:41 PM
+	 Created by:   	dschauland
+	 Organization: 	
+	 Filename:     	MVPContributions.psm1
+	-------------------------------------------------------------------------
+	 Module Name: MVPContributions
+	===========================================================================
+#>
+
+
+
+function Write-HelloWorld
+{
+	Write-Host "Hello World"
+}
+
+Export-ModuleMember -Function Write-HelloWorld
+
+
+
